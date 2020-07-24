48e6a5d466770cf60fcec077d0dec447015772ab
Ivan <ivan@vtolm.ru>
2020-07-24 14:28:10 +0300

    Add Demo

---
+++ PiedPiperDemo/CometChatInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CometChatFormSample
+{
+    public interface CometChatInterface
+    {
+        void initializeCometChat(string SiteUrl,String LicenseKey, String ApiKey,Boolean isCometOnDemand,CometChatFormCallback callback);
+        void loginWithUID(string UID, CometChatFormCallback callback);
+        void launchCometChatWindow(Boolean isFullScreen, CometChatLaunchCallbacks launchCallbacks);
+        void launchCometChatWithId(Boolean isFullScreen,String userOrGroupId,Boolean isGroup,Boolean setBackButton,CometChatLaunchCallbacks launchCallbacks);
+    }
+}
